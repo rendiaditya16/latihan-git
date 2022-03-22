605822abeaf947583d6bb26907548394fafbd797
rendiaditya16 <rendi.aditya221@gmail.com>
2022-03-22 15:58:41 +0700

    menambahkan Biodata.php

---
+++ Biodata.php
@@ -0,0 +1,21 @@
+<?php
+class Biodata extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN </h1>";
+        echo " <p> BIODATA DIRI SAYA</p>";
+        echo " <p> NAMA                : RENDI ADITYA PUTRA</p>";
+        echo " NIM                     : 17190230 ";
+        echo " <p> KELAS               : 17.6A.25 </p>";
+        echo " Fakultas                : Teknologi INformasi";
+        echo " <p> UNIVERSITAS         : BINA SARANA INFORMATIKA</p>";
+        echo " JENIS KELAMIN           : LAKI-LAKI";
+        echo " <p>TEMPAT,TANGGAL LAHIR : JAKARTA, 20 NOVEMBER 2000</p>";
+        echo " GOLONGAN DARAH          : A";
+        echo " <p>ALAMAT               : JALAN KEBON 200 RT 006/002 NO.26 </p>";
+        echo " HOBBY                   : TRAVELLING, BERMAIN GAME, NGODING ";
+        echo " <p> E-MAIL              : rendi.aditya221@gmail.com</p>";
+        echo " NO.HP                   : 089602992152";
+    }
+}
