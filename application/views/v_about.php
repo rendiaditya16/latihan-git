f85597a00a077b2e12cb0e7ecd8a4742469c4735
rendiaditya16 <rendi.aditya221@gmail.com>
2022-04-05 18:54:57 +0700

    Membuat tampilan web sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Rendi</li>
+ <li>Nama Belakang : Aditya</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Kebon Dua Ratus RT 06 RW 02 NO.26 Kamal </li>
+ </ul>
+
+ <h4>Tempat Lahir, Tanggal Lahir</h4>
+ <ul type="none">
+ <li>Jakarta, 20 November 2000</li>
+ </ul>
+ <h4>MY HOBBY</h4>
+ <ul type="square">
+ <li>Travelling </li>
+ <li>Bermain Game Aov</li>
+ </ul>
+</section>
